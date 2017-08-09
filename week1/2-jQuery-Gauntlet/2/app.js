9a843ed10d5328d573f8d44c68f58d78009526c3
Georgi Mavridis <georgi@ticktech.bg>
2017-08-09 17:32:41 +0300

    added solution to week1/2-jQuery-Gauntlet/2

--- week1/2-jQuery-Gauntlet/2/app.js
+++ week1/2-jQuery-Gauntlet/2/app.js
@@ -1,1 +1,15 @@
-console.log("Empty I am.");
+$("#toggleHighlight").click(function () {
+	var highlighted = $("p.highlight");
+
+	if(highlighted.length == 0) {
+		$("p.first").addClass('highlight');
+	} else {
+		var next = $("p.highlight").next(); 
+		$("p.highlight").removeClass('highlight');
+		if(next.length > 0) {
+			next.addClass('highlight');
+		} else {
+			$("p.first").addClass('highlight');
+		}	
+	}
+})
